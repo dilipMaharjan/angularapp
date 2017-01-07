43c9130a8566f248851311810d7cb0705c5237ba
crazdrms <dilipmlove@gmail.com>
2017-01-07 17:57:48 +0545

    dynamic services and locations from json file

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
 var app = angular.module("app", ['ngRoute']);
-app.config(function ($routeProvider) {
-    $routeProvider
-            .when('/', {templateUrl: 'partials/main.html'})
-            .when('/about', {templateUrl: 'partials/about.html'})
-            .when('/services', {templateUrl: 'partials/services.html'})
-            .when('/contact', {templateUrl: 'partials/contact.html'})
-            .otherwise('/');
-});
+app.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+                .when('/', {
+                    templateUrl: 'partials/main.html',
+                    controller: 'ServicesController'
+                })
+                .when('/about', {
+                    templateUrl: 'partials/about.html'
+                })
+                .when('/services', {
+                    templateUrl: 'partials/services.html',
+                    controller: 'ServicesController'
+                })
+                .when('/contact', {
+                    templateUrl: 'partials/contact.html',
+                    controller: 'ContactController'
+                })
+                .otherwise('/');
+    }])
+        .controller('ServicesController', [
+            '$scope', '$http', function ($scope, $http) {
+                $http.get('services.json').then(function (response) {
+                    $scope.services = response.data;
+                });
+            }
+        ])
+        .controller('ContactController', [
+            '$scope', '$http', function ($scope, $http) {
+                $http.get('locations.json').then(function (response) {
+                    $scope.locations = response.data;
+                });
+            }
+        ]);
 
